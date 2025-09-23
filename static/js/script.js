7a7c70901131758b438f6532cb7facfd11661419
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2025-09-23 08:15:26 -0700

    Mi primer commit de menu de actividades;

---
+++ static/js/script.js
@@ -0,0 +1,56 @@
+function mostrarTareas(mes) {
+  const tareas = {
+    septiembre: [
+      { texto: "HTML - CSS - JS - Audio", url: "https://misitio.com/ej-1" },
+      { texto: "HTML - CSS - JS - Video", url: "https://misitio.com/ej-2" },
+    ],
+    octubre: [
+      {
+        texto: "HTML - CSS - JS - Multiples Imagenes",
+        url: "https://misitio.com/ej-3",
+      },
+      {
+        texto: "HTML - CSS - JS - Combinacion de Objetos",
+        url: "https://misitio.com/ej-4",
+      },
+    ],
+    noviembre: [
+      {
+        texto: "HTML - CSS - JS - Responsivos",
+        url: "https://misitio.com/ej-5",
+      },
+      {
+        texto: "HTML - CSS - JS - Animaciones",
+        url: "https://misitio.com/ej-6",
+      },
+    ],
+  };
+
+  const lista = document.querySelector("#lista-tareas");
+  lista.innerHTML = "";
+  lista.classList.add("oculto");
+
+  tareas[mes].forEach((tarea) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="${tarea.url}" target="_blank">${tarea.texto}</a>`;
+    lista.appendChild(li);
+  });
+
+  lista.classList.remove("oculto");
+}
+
+function mostrarMensaje() {
+  const comentario = document.querySelector("#comentario").value;
+  if (comentario.replace(/ /g, "") === "") {
+    alert("Pofavor escribe un comentario antes de enviar!!!");
+  } else {
+    alert(`Gracias por tu retroalimentación :)`);
+  }
+  document.querySelector("#comentario").value = "";
+}
+
+function ocultarLista() {
+  const lista = document.querySelector("#lista-tareas");
+  lista.classList.add("oculto");
+  lista.innerHTML = "";
+}
